a2055b6a3330d547d9e35e3a40d50b60edfd5bde
jaimelgc <jaimeleongarcia123@gmail.com>
2024-02-04 21:57:23 +0000

    url media

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -10,6 +11,12 @@ class Category(models.Model):
         indexes = [models.Index(fields=['name'])]
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse(
+            'shop:product_list_by_category',
+            args=[self.slug],
+        )
 
     def __str__(self):
         return self.name
@@ -43,6 +50,12 @@ class Product(models.Model):
             models.Index(fields=['name']),
             models.Index(fields=['-created']),
         ]
+
+    def get_absolute_url(self):
+        return reverse(
+            'shop:product_detail',
+            args=[self.id, self.slug],
+        )
 
     def __str__(self):
         return self.name
