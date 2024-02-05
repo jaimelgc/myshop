fae741d89687cae5a7963bd7ad3b43a1c35bcdd4
jaimelgc <jaimeleongarcia123@gmail.com>
2024-02-05 10:43:59 +0000

    chapter 9

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from .models import Category, Product
 
 
@@ -11,7 +10,8 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_display = ['name', 'slug', 'price',
+                    'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name',)}
