7789f96a436352de2ea4a4e3ac981c1bee03aeb3
jaimelgc <jaimeleongarcia123@gmail.com>
2024-02-05 02:07:34 +0000

    updating product quantities in cart

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 
+from cart.forms import CartAddProductForm
+
 from .models import Category, Product
 
 
@@ -23,10 +25,12 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
     return render(
         request,
         'shop/product/detail.html',
-        {'product': product},
+        {
+            'product': product,
+            'cart_product_form': cart_product_form,
+        },
     )
-
-
