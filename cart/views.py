23d749911143879f98c092be276de2cd295fc1d3
jaimelgc <jaimeleongarcia123@gmail.com>
2024-02-05 10:57:55 +0000

    chapter 10

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
-
 from shop.models import Product
-
+from coupons.forms import CouponApplyForm
+from shop.recommender import Recommender
 from .cart import Cart
 from .forms import CartAddProductForm
 
@@ -14,11 +14,9 @@ def cart_add(request, product_id):
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
-        cart.add(
-            product=product,
-            quantity=cd['quantity'],
-            override_quantity=cd['override'],
-        )
+        cart.add(product=product,
+                 quantity=cd['quantity'],
+                 override_quantity=cd['override'])
     return redirect('cart:cart_detail')
 
 
@@ -33,10 +31,21 @@ def cart_remove(request, product_id):
 def cart_detail(request):
     cart = Cart(request)
     for item in cart:
-        item['update_quantity_form'] = CartAddProductForm(
-            initial={
-                'quantity': item['quantity'],
-                'override': True,
-            }
-        )
-    return render(request, 'cart/detail.html', {'cart': cart})
+        item['update_quantity_form'] = CartAddProductForm(initial={
+                            'quantity': item['quantity'],
+                            'override': True})
+    coupon_apply_form = CouponApplyForm()
+
+    r = Recommender()
+    cart_products = [item['product'] for item in cart]
+    if(cart_products):
+        recommended_products = r.suggest_products_for(cart_products,
+                                                      max_results=4)
+    else:
+        recommended_products = []
+
+    return render(request,
+                  'cart/detail.html',
+                  {'cart': cart,
+                   'coupon_apply_form': coupon_apply_form,
+                   'recommended_products': recommended_products})
--- myshop/settings.py
+++ myshop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
     'payment.apps.PaymentConfig',
+    'coupons.apps.CouponsConfig',
 ]
 
 MIDDLEWARE = [
@@ -143,3 +144,9 @@ STRIPE_PUBLISHABLE_KEY = '' # Publishable key
 STRIPE_SECRET_KEY = ''      # Secret key
 STRIPE_API_VERSION = '2022-08-01'
 STRIPE_WEBHOOK_SECRET = ''
+
+
+# Redis settings
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 1
